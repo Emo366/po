894e505636d879e3c87f05c3f7cd33667dec4e40
emo366 <73887722+Emo366@users.noreply.github.com>
2024-12-26 03:14:08 +0400

    change

--- functions/location.js
+++ functions/location.js
@@ -1,19 +1,3 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const app = express();
-// app.use(bodyParser.json());
-
-// app.post('/location', (req, res) => {
-//   const { latitude, longitude } = req.body;
-//   console.log(`Местоположение: широта ${latitude}, долгота ${longitude}`);
-//   res.send('Местоположение получено.');
-// });
-
-// app.listen(3000, () => {
-//   console.log('Сервер запущен на порту 3000');
-// });
-
 exports.handler = async (event, context) => {
   if (event.httpMethod === 'POST') {
     const { latitude, longitude } = JSON.parse(event.body);
